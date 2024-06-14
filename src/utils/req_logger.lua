aae06ccc21eb61cd56cb9667fd28d8207fd24ea4
RainyXeon / <minh15052008@gmail.com>
2024-06-14 17:51:32 +0700

    fix: false logic for logging headers

--- src/utils/req_logger.lua
+++ src/utils/req_logger.lua
@@ -4,7 +4,7 @@ return function (req, res, go)
   if not config.logger.request.enable then go() end
   go()
   print(string.format("[webserver]: %s %s %s", res.code, req.method, req.path))
-  if not config.logger.request.withHeader then
+  if config.logger.request.withHeader then
     p(req.headers)
   end
 end
